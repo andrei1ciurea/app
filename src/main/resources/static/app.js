ed19dd8952ca9f7114d13d17f59575257a3cb6a4
Andrei Ciurea <andrei.ciurea@endava.com>
2017-10-02 17:37:34 +0300

    ui work

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,8 +1,15 @@
 angular.module('demo', [])
-    .controller('appController', function($scope, $http) {
-        $http.get('http://localhost:8080/activity/all').
-        then(function(response) {
+    .controller('actController', function ($scope, $http) {
+        $http.get('http://localhost:8080/activity/all').then(function (response) {
             $scope.activities = response.data;
             console.log($scope.activities)
         });
+    })
+    .controller('btnController', function ($scope, $http) {
+        $scope.Clicka = function(){
+            $http.get('http://localhost:8080/activity/add?name=\"' + $scope.name + '\"\&details=\"' + $scope.details+"\"").then(function (response) {
+                $scope.activities = response.data;
+                console.log($scope.activities)
+            });
+        }
     });
